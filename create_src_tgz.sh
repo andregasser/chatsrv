73633dfdd72718f9a865c9613de59767291a3ea3
André Gasser <andre.gasser@gmx.ch>
2012-06-09 14:35:33 +0200

    Modified tar.gz builder script

--- create_src_tgz.sh
+++ create_src_tgz.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-tar --create --file=chatsrv-0.4.tar chatsrv.c llist2.c llist2.h log.c log.h bool.h Makefile COPYING README
-gzip chatsrv-0.4.tar
+tar --create --file=chatsrv-0.5.tar chatsrv.c llist2.c llist2.h log.c log.h bool.h colors.h Makefile COPYING README
+gzip chatsrv-0.5.tar
